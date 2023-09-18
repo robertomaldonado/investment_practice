a536b3d47db8eb27321ecc4769d7a4cb25736345
Roberto <rbt.maldonado@gmail.com>
2023-09-18 10:45:55 -0500

    Broke into comps

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import logo from "../assets/investment-calculator-logo.png";
+
+function Header() {
+  return (
+    <div>
+      <header className="header">
+        <img src={logo} alt="logo" />
+        <h1>Investment Calculator</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
