61d6332b41cdb637ff167e7c4ac8e2046f013c1e
Roberto <rbt.maldonado@gmail.com>
2023-09-19 19:43:12 -0500

    Conditional output for results added

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,35 +7,8 @@ const initialUserInput = {
   duration: 10,
 };
 
-function Form() {
+const Form = (props) => {
   const [userInput, setUserInput] = useState(initialUserInput);
-
-  const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
-
-    const yearlyData = []; // per-year results
-
-    let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
-    const expectedReturn = +userInput["expected-return"] / 100;
-    const duration = +userInput["duration"];
-
-    // The below code calculates yearly results (total savings, interest etc)
-    for (let i = 0; i < duration; i++) {
-      const yearlyInterest = currentSavings * expectedReturn;
-      currentSavings += yearlyInterest + yearlyContribution;
-      yearlyData.push({
-        // feel free to change the shape of the data pushed to the array!
-        year: i + 1,
-        yearlyInterest: yearlyInterest,
-        savingsEndOfYear: currentSavings,
-        yearlyContribution: yearlyContribution,
-      });
-    }
-
-    // do something with yearlyData ...
-  };
 
   const resetHandler = () => {
     setUserInput(initialUserInput);
@@ -43,7 +16,7 @@ function Form() {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    calculateHandler(userInput);
+    props.onCalculate(userInput);
   };
 
   const changeHandler = (input, value) => {
@@ -120,6 +93,6 @@ function Form() {
       </form>
     </div>
   );
-}
+};
 
 export default Form;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,3 +1,10 @@
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
 const Table = (props) => {
   return (
     <div>
@@ -12,13 +19,26 @@ const Table = (props) => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
+          {props.data.map((yearData) => (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.savingsEndOfYear)}</td>
+              <td>{formatter.format(yearData.yearlyInterest)}</td>
+              <td>
+                {formatter.format(
+                  yearData.savingsEndOfYear -
+                    props.initialInvestment -
+                    yearData.yearlyContribution * yearData.year
+                )}
+              </td>
+              <td>
+                {formatter.format(
+                  props.initialInvestment +
+                    yearData.yearlyContribution * yearData.year
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
